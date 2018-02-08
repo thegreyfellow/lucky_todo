960e95f7ebffe51942c8ee855365ff389f994c44
Youssef Idmoussi <youssef.idmoussi@gmail.com>
2018-02-08 11:16:09 +0000

    Add table Todo with title and description.

---
+++ db/migrations/20180208102629_create_todos.cr
@@ -0,0 +1,18 @@
+class CreateTodos::V20180208102629 < LuckyMigrator::Migration::V1
+  def migrate
+    create :todos do
+     add title : String
+     add description : String?
+
+     add_index :title
+    end
+
+    # Run custom SQL with execute
+    #
+    # execute "CREATE UNIQUE INDEX things_title_index ON things (title);"
+  end
+
+  def rollback
+    # drop :things
+  end
+end
