156179e4e2f2824403447030e08fbd0620bf7d53
Youssef Idmoussi <youssef.idmoussi@gmail.com>
2018-02-11 16:10:28 +0000

    Remove unused Errors::Show render cases and only leave json.

--- src/actions/errors/show.cr
+++ src/actions/errors/show.cr
@@ -1,23 +1,19 @@
 class Errors::Show < Lucky::ErrorAction
   def handle_error(error : JSON::ParseException)
-    if json?
-      message = "There was a problem parsing the JSON." \
-                " Please check that it is formed correctly"
-      json Errors::ShowSerializer.new(message), status: 400
-    else
-      render_error_page status: 500
-    end
+    message = "There was a problem parsing the JSON." \
+              " Please check that it is formed correctly"
+    json Errors::ShowSerializer.new(message), status: 400
+  end
+
+  def handle_error(error : LuckyRecord::RecordNotFoundError)
+    message = error.message.to_s
+    json Errors::ShowSerializer.new(message), status: 404
   end
 
   def handle_error(error : Exception)
     error.inspect_with_backtrace(STDERR)
-
-    if json?
-      message = "An unexpected error occurred"
-      json Errors::ShowSerializer.new(message), status: 500
-    else
-      render_error_page status: 500
-    end
+    message = "An unexpected error occurred"
+    json Errors::ShowSerializer.new(message), status: 500
   end
 
   private def render_error_page(status : Int32, title : String = "We're sorry. Something went wrong.")
